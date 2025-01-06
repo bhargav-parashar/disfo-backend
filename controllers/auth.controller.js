daf9b3fe3747c2dad1597ac2077aaca0fd7c995b
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-01-06 20:09:30 +0530

    Hash password added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,28 @@
 const UserService = require("../services/user.service");
 const UserServiceInstance = new UserService();
+const AuthService = require("../services/auth.service");
+const AuthServiceInstance = new AuthService();
 
-const postSignUp = async (req,res) =>{
-    try{
-        res.status(201).send(await UserServiceInstance.register(req.body));     
-    }catch(err){
-        res.status(500).send({message:"Something went wrong",err})
-    }
+const postSignUp = async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+
+  try {
+    const passwordHash = await AuthServiceInstance.generatePasswordHash(
+      req.body.password
+    );
+
+    const newUser = await UserServiceInstance.register({
+      ...req.body,
+      password: passwordHash,
+    });
+
+    res.status(201).send(newUser);
+    
+  } catch (err) {
+    res.status(500).send({ message: "Something went wrong", err });
+  }
 };
 
-const postLogin = (req,res) =>{};
+const postLogin = (req, res) => {};
 
 module.exports = { postSignUp, postLogin };
---
+++ services/auth.service.js
@@ -0,0 +1,7 @@
+const bcrypt = require("bcrypt");
+
+class AuthService{
+    generatePasswordHash = (plainTextPassword) => bcrypt.hash(plainTextPassword,10);
+}
+
+module.exports = AuthService;
